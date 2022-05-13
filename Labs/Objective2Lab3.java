f238878aef2af7f601a53a0b2d1c895f4029942c
GoldilocksZone <jmarulli@gmail.com>
2022-05-13 09:52:47 -0400

    Completed Objective 2 - Lab 3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,9 @@
+public class Objective2Lab3 {
+	public static void main (String[] args) {
+		double num1 = 4, num2 = 5, num3 = 9, average = (num1 + num2 + num3)/3;
+
+		System.out.println("(" + num1 + " + " + num2 + " + " + num3 + ")/3 = " + average);
+	}
+}
+
+		
